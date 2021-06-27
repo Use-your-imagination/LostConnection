e87c40de525807c8974f71f01f69eba865c2d832
LazyPanda07 <semengricenko@gmail.com>
2021-06-28 04:43:51 +0500

    Refactor

--- Source/LostConnection/LostConnectionGameMode.cpp
+++ Source/LostConnection/LostConnectionGameMode.cpp
@@ -1,14 +1,13 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+#include "LostConnectionGameMode.h"
 
 #include "UObject/ConstructorHelpers.h"
 
-#include "LostConnectionGameMode.h"
 #include "LostConnectionCharacter.h"
 
 ALostConnectionGameMode::ALostConnectionGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/LostConnectionCPP/Blueprints/LostConnectionCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
