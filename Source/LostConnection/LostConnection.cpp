9e1cbb98c2f5d0a0e664341c33e344082a432e1e
LazyPanda07 <semengricenko@gmail.com>
2021-06-28 01:38:53 +0500

    Init

---
+++ Source/LostConnection/LostConnection.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LostConnection.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LostConnection, "LostConnection" );
+ 
