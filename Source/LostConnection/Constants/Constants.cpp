bccfbe779cd4ee1f09113b5538a27b08817dd668
LazyPanda07 <semengricenko@gmail.com>
2022-03-01 01:20:24 +0500

    Add new configuration

--- Source/LostConnection/Constants/Constants.cpp
+++ Source/LostConnection/Constants/Constants.cpp
@@ -2,7 +2,7 @@
 
 #include "Constants.h"
 
-const FString UConstants::projectVersion = "v0.2.0.0";
+const FString UConstants::projectVersion = "v0.2.1.0";
 
 const FName UConstants::actionStringTablePath = "/Game/Text/Actions.Actions";
 const FString UConstants::defaultActionMessageKey = "DefaultActionMessage";
