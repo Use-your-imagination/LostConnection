b319792f8e4f803f758b16198cd24e9f1d74ad15
LazyPanda07 <semengricenko@gmail.com>
2022-03-12 02:56:47 +0500

    Add loot points cost

--- Source/LostConnection/Loot/LootManager.cpp
+++ Source/LostConnection/Loot/LootManager.cpp
@@ -2,7 +2,12 @@
 
 #include "LootManager.h"
 
+#include "Engine/LostConnectionPlayerState.h"
+#include "Constants/Constants.h"
+
 void ALootManager::addRandomWeapon_Implementation(UInventory* playerInventory)
 {
+	playerInventory->getPlayerState()->spendLootPoints(FMath::Min(playerInventory->getLootPoints(), UConstants::maxSpendLootPoints));
+
 	lootCreator.createRandomWeapon(playerInventory);
 }
