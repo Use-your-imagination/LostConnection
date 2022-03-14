e3fa5dcac4d81c5a051d5991e4c9f651910d191e
LazyPanda07 <semengricenko@gmail.com>
2022-03-14 22:12:52 +0500

    Remove ICooldownableHolder

--- Source/LostConnection/Interfaces/Holders/CooldownableHolder.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2022 Use-your-imagination
-
-
-#include "Interfaces/Holders/CooldownableHolder.h"
-
-// Add default functionality here for any ICooldownableHolder functions that are not pure virtual.
