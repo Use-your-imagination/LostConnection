b0c35ad3d72aa64e7bf358825820c5b7c9b939d5
LazyPanda07 <semengricenko@gmail.com>
2022-06-28 21:30:44 +0500

    Refactor

--- Source/LostConnection/Interfaces/Gameplay/Descriptions/Caster.cpp
+++ Source/LostConnection/Interfaces/Gameplay/Descriptions/Caster.cpp
@@ -7,7 +7,7 @@
 
 void ICaster::castAbility(UBaseAbility* ability, const TFunction<void()>& callback)
 {
-	AActor* actor = Cast<AActor>(this->_getUObject());
+	TObjectPtr<AActor> actor = Cast<AActor>(this->_getUObject());
 
 	Utility::executeOnlyOnServerFromMulticast(actor, [this, ability, callback]()
 		{
@@ -46,18 +46,18 @@ void ICaster::setCurrentAbility(UBaseAbility* ability)
 
 const TArray<UAnimMontage*>& ICaster::getAbilitiesAnimations() const
 {
-	static TArray<UAnimMontage*> filler;
+	static TArray<TObjectPtr<UAnimMontage>> filler;
 
 	PURE_VIRTUAL(ICaster::getAbilitiesAnimations, return filler;);
 }
 
 void ICaster::cancelCurrentAbilityAnimation()
 {
-	ABaseDrone* drone = Cast<ABaseDrone>(this);
+	TObjectPtr<ABaseDrone> drone = Cast<ABaseDrone>(this);
 
 	if (drone)
 	{
-		UBaseAbility* currentAbility = drone->getCurrentAbility();
+		TObjectPtr<UBaseAbility> currentAbility = drone->getCurrentAbility();
 
 		if (currentAbility && currentAbility->getIsCancelable())
 		{
@@ -69,11 +69,11 @@ void ICaster::cancelCurrentAbilityAnimation()
 		return;
 	}
 
-	ABaseCasterBot* bot = Cast<ABaseCasterBot>(this);
+	TObjectPtr<ABaseCasterBot> bot = Cast<ABaseCasterBot>(this);
 
 	if (bot)
 	{
-		UBaseAbility* currentAbility = bot->getCurrentAbility();
+		TObjectPtr<UBaseAbility> currentAbility = bot->getCurrentAbility();
 
 		if (currentAbility && currentAbility->getIsCancelable())
 		{
@@ -88,33 +88,27 @@ TArray<TWeakObjectPtr<UBaseAbility>> ICaster::getAbilities() const
 {
 	TArray<TWeakObjectPtr<UBaseAbility>> result;
 
-	UBaseAbility* passive = this->getPassiveAbility();
-	UBaseAbility* first = this->getFirstAbility();
-	UBaseAbility* second = this->getSecondAbility();
-	UBaseAbility* third = this->getThirdAbility();
-	UBaseAbility* ultimate =this->getUltimateAbility();
-
-	if (passive)
+	if (TObjectPtr<UBaseAbility> passive = this->getPassiveAbility())
 	{
 		result.Add(passive);
 	}
 
-	if (first)
+	if (TObjectPtr<UBaseAbility> first = this->getFirstAbility())
 	{
 		result.Add(first);
 	}
 
-	if (second)
+	if (TObjectPtr<UBaseAbility> second = this->getSecondAbility())
 	{
 		result.Add(second);
 	}
 
-	if (third)
+	if (TObjectPtr<UBaseAbility> third = this->getThirdAbility())
 	{
 		result.Add(third);
 	}
 
-	if (ultimate)
+	if (TObjectPtr<UBaseAbility> ultimate = this->getUltimateAbility())
 	{
 		result.Add(ultimate);
 	}
